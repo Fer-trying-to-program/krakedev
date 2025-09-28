1aa8749ed6608d4db67d39ba3dc2e87115fc9601
Fer-trying-to-program <fer.rojitas.abad2001@.com>
2025-09-28 16:22:21 -0500

    creacion del archivo js utilitarios

--- Utilitarios/Saludo.js
+++ Utilitarios/Saludo.js
@@ -7,31 +7,3 @@ saludar=function(){
     mostrarImagen("img","./Imagenes/baldi-playtime.gif");
     mostrarTextoEnCaja("txtNombre","");
 }
-recuperarTexto=function(idComponente){
-    let guardar
-    let recuperar=document.getElementById(idComponente);
-    guardar=recuperar.value;
-    return guardar;
-}
-transformarInt=function(idComponente){
-    let guardar=recuperarTexto(idComponente);
-    let Int=parseInt(guardar);
-    return Int;
-}
-transformarFloat=function(idComponente){
-    let guardar=recuperarTexto(idComponente);
-    let float=parseFloat(guardar);
-    return float;
-}
-mostrarTexto=function(idComponente,texto){
-    let recuperar=document.getElementById(idComponente);
-    recuperar.innerText=texto;
-}
-mostrarImagen=function(idComponente,Img){
-    let recuperar=document.getElementById(idComponente);
-    recuperar.src=Img;
-}
-mostrarTextoEnCaja=function(idComponente,texto){
-    let guardar=document.getElementById(idComponente);
-    guardar.value=texto;
-}
