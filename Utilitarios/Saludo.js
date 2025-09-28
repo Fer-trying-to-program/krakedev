6e98320709ea2494e9fcdf6e112bf023660e7859
Fer-trying-to-program <fer.rojitas.abad2001@.com>
2025-09-28 15:47:46 -0500

    Funcion int y float agregadas correctamente

--- Utilitarios/Saludo.js
+++ Utilitarios/Saludo.js
@@ -1,10 +1,22 @@
 saludar=function(){
     let nombre=recuperarTexto("txtNombre");
     let apellido=recuperarTexto("txtApellido");
+    let edad=transformarInt("txtEdad");
+    let estatura=transformarFloat("txtEstatura");
 }
 recuperarTexto=function(idComponente){
     let guardar
     let recuperar=document.getElementById(idComponente);
     guardar=recuperar.value;
     return guardar;
+}
+transformarInt=function(idComponente){
+    let guardar=recuperarTexto(idComponente);
+    let Int=parseInt(guardar);
+    return Int;
+}
+transformarFloat=function(idComponente){
+    let guardar=recuperarTexto(idComponente);
+    let float=parseFloat(guardar);
+    return float;
 }
