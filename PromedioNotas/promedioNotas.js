62cbef508c9d3c33f90855f6db0f1d0f28145458
Fer-trying-to-program <fer.rojitas.abad2001@.com>
2025-10-01 12:24:27 -0500

    Archivo Promedio Notas funciona correctamente, falta agregar if

---
+++ PromedioNotas/promedioNotas.js
@@ -0,0 +1,11 @@
+calcularPromedioNotas=function(){
+    let guardar1=recuperarFlotante("nota1");
+    let guardar2=recuperarFlotante("nota2");
+    let guardar3=recuperarFlotante("nota3");
+    let redondear
+    let promedio
+    promedio=calcularPromedio(guardar1,guardar2,guardar3);
+    redondear=promedio.toFixed(2);
+    cambiarTexto("txtPromedio", redondear);
+
+}
---
+++ PromedioNotas/servicioPromedioNotas.js
@@ -0,0 +1,7 @@
+calcularPromedio=function(n1,n2,n3){
+    let suma
+    let promediar
+    suma=n1+n2+n3;
+    promediar=suma/3;
+    return promediar;    
+}
