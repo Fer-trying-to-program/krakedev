06fb14fe971e1610f0eabdf7b41afe423e0a18f0
Fer-trying-to-program <fer.rojitas.abad2001@.com>
2025-10-01 12:29:27 -0500

    Se implemento el if, con imagenes, todo funciona

--- PromedioNotas/promedioNotas.js
+++ PromedioNotas/promedioNotas.js
@@ -7,5 +7,10 @@ calcularPromedioNotas=function(){
     promedio=calcularPromedio(guardar1,guardar2,guardar3);
     redondear=promedio.toFixed(2);
     cambiarTexto("txtPromedio", redondear);
+    if(promedio>7){
+        cambiarImagen("imgPromedio","basado.gif");
+        }else{
+            cambiarImagen("imgPromedio","payaso.gif");
+    }
 
 }
