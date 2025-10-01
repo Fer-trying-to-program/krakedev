f72d88101f854b3c9efc98dd0364be1704a0922c
Fer-trying-to-program <fer.rojitas.abad2001@.com>
2025-10-01 11:55:13 -0500

    La pagina dados, esta funcionando correctamente

---
+++ DadosArchivos/dados.js
@@ -0,0 +1,20 @@
+jugar=function(){
+   let guardar=lanzarDado();  
+   cambiarTexto("lblNumero",guardar);
+   if(guardar>3){
+        cambiarTexto("txtIf","Es mayor a 3, ganaste");
+   }else{
+        cambiarTexto("txtIf","Es menor a 3, perdiste");
+
+   }
+}
+lanzarDado=function(){
+    let guardar
+    let numeroMultiplicado
+    let float
+    guardar=Math.random();
+    numeroMultiplicado=guardar*7;
+    float=parseInt(numeroMultiplicado);
+    console.log(float);
+    return float;
+}
