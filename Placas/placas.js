45e0ae043858e81b2ea11a35685b020e39fcfeec
Fer-trying-to-program <fer.rojitas.abad2001@.com>
2025-10-13 19:01:33 -0500

    La pagina de verificacion de placas funciona, archivo word con casos subido

--- Placas/placas.js
+++ Placas/placas.js
@@ -1,12 +1,28 @@
+
 validarPlaca=function(){
     let valorIngresado=recuperarTexto("txtPrueba");
     let validacion
+    let provincia
     validacion=validarEstructura(valorIngresado);
-    if(validarEstructura(valorIngresado) ==false){
+    provincia=obtenerProvincia(valorIngresado);
+    if(validacion==false){
         mostrarTexto("txtValidacion", "la placa es valida");
-        let guardar=obtenerProvincia(valorIngresado);
-    }else{
+    if(provincia==true){
+        obtenerDiaPicoYPlaca(valorIngresado);
+    }
+    if(provincia==false){
         mostrarTexto("txtValidacion","la placa no es valida");
+        mostrarTexto("txtPicoYPlaca","");
         mostrarTexto("txtPlacas","Vehiculo incorrecto");
     }
-}  
+}
+}
+    
+
+limpiar=function(){
+    mostrarTexto("txtError","");
+    mostrarTexto("txtPlacas","");
+    mostrarTexto("txtPicoYPlaca","");
+    mostrarTexto("txtValidacion","");
+    mostrarTextoEnCaja("txtPrueba","");
+} 
--- Placas/servicioPlacas.js
+++ Placas/servicioPlacas.js
@@ -32,11 +32,11 @@ validarEstructura=function(placa){
     }else if(char4!="-"){
         mostrarTexto("txtError","El cuarto caracter debe ser un guion");
     
-    }else if(char5<0 | char5>9 | isNaN(char5)){
+    }else if(char5<0 | char5>9 | isNaN(char5) | char5==""){
         mostrarTexto("txtError","El quinto caracter deber ser un numero entre 0 y 9");
-    }else if(char6<0 | char6>9 | isNaN(char6)){
+    }else if(char6<0 | char6>9 | isNaN(char6) | char6==""){
         mostrarTexto("txtError","El sexto caracter deber ser un numero entre 0 y 9");
-    }else if(char7<0 | char7>9 | isNaN(char7)){
+    }else if(char7<0 | char7>9 | isNaN(char7) | char7==""){
         mostrarTexto("txtError","El septimo caracter deber ser un numero entre 0 y 9");
     }else if(char8<0 | char8>9 | isNaN(char8)){
         mostrarTexto("txtError","El octavo caracter deber ser un numero entre 0 y 9")
@@ -48,7 +48,7 @@ validarEstructura=function(placa){
             char6>="0" & char6<="9" & char7>="0" & char7<="9" || char1>="A" & char1<="Z" & char2>="A" & char2<="Z" & char3>="A" & char3<="Z" & char4=="-" & char5>="0" & char5<="9" & 
             char6>="0" & char6<="9" & char7>="0" & char7<="9"& char8>="0" & char8<="9"){
                 
-                return null,false;
+                return false;
             }
         }
 }
@@ -57,52 +57,91 @@ let char
 char=placa.charAt(0);
 if(char=="A"){
     mostrarTexto("txtPlacas","Su placa es de la provincia de Azuay");
+    return true;
 }else if(char=="B"){
     mostrarTexto("txtPlacas","Su placa es de la provincia de Bolizar");
+    return true;
 }else if(char=="U"){
     mostrarTexto("txtPlacas","Su placa es de la provincia de Cañar");
+    return true;
 }else if(char=="C"){
     mostrarTexto("txtPlacas", "su placa es de la provincia de Carchi");
+    return true;
 }else if(char=="X"){
     mostrarTexto("txtPlacas","Su placa es de la provincia de Cotopaxi");
+    return true;
 }else if(char=="H"){
     mostrarTexto("txtPlacas","Su placa es de la provincia de chimborazo");
+    return true;
 }else if(char=="O"){
     mostrarTexto("txtPlacas","Su placa es de la provincia de El Oro");
+    return true;
 }else if(char=="E"){
     mostrarTexto("txtPlacas","Su placa es de la provincia de Esmeraldas");
+    return true;
 }else if(char=="W"){
     mostrarTexto("txtPlacas","Su placa es de la provincia de Galapagos");
+    return true;        
 }else if(char=="G"){
     mostrarTexto("txtPlacas","Su placa es de la provincia del Guayas");
+    return true;
 }else if(char=="I"){
     mostrarTexto("txtPlacas","Su placa es de la provincia de Imbabura");
+    return true;
 }else if(char=="L"){
     mostrarTexto("txtPlacas","Su placa es de la provincia de Loja");
+    return true;
 }else if(char=="R"){
     mostrarTexto("txtPlacas","Su placa es de la provincia de Los Rios");
+    return true;
 }else if(char=="M"){
     mostrarTexto("txtPlacas","Su placa es de la provincia de Manabi");
+    return true;
 }else if(char=="V"){
     mostrarTexto("txtPlacas","Su placa es de la provincia de Morona Santiago");
+    return true;
 }else if(char=="N"){
     mostrarTexto("txtPlacas","Su placa es de la provincia de Napo");
+    return true;
 }else if(char=="S"){
     mostrarTexto("txtPlacas","Su placa es de la provincia de Pastaza");
+    return true;
 }else if(char=="P"){
     mostrarTexto("txtPlacas","Su placa es de la provincia de Pichincha");
+    return true;
 }else if(char=="K"){
     mostrarTexto("txtPlacas","Su placa es de la provincia de Sucumbios");
+    return true;
 }else if(char=="Q"){
     mostrarTexto("txtPlacas","Su placa es de la provincia de Orellana");
+    return true;
 }else if(char=="T"){
     mostrarTexto("txtPlacas","Su placa es de la provincia de Tungurahua");
+    return true;
 }else if(char=="Z"){
     mostrarTexto("txtPlacas","Su placa es de la provincia de Zamora Chinchipe");
+    return true;
 }else if(char=="Y"){
     mostrarTexto("txtPlacas","Su placa es de la provincia de Santa Elena");
+    return true;
 }else{
-    mostrarTexto("txtplacas","");
-    return null,false;
+    mostrarTexto("txtPlacas","Letra no valida");
+    return false
+
 }
 }
+obtenerDiaPicoYPlaca=function(placa){
+    let longitud=placa.length-1;
+    let char=placa.charAt(longitud);
+    if(char==1 || char==2){
+        mostrarTexto("txtPicoYPlaca","El lunes tiene pico y placa");
+    }else if(char==3 || char==4){
+        mostrarTexto("txtPicoYPlaca","El martes tiene pico y placa");
+        }else if(char==5 || char==6){
+        mostrarTexto("txtPicoYPlaca","El miercoles tiene pico y placa");
+        }else if(char==7 || char==8){
+        mostrarTexto("txtPicoYPlaca","el jueves tiene pico y placa");
+        }else if(char==9 || char==0){
+        mostrarTexto("txtPicoYPlaca","El viernes tiene pico y placa");
+        }
+    }
