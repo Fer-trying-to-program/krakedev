30f04e0aae17e26556501dda7f3f12c182b16524
Fer-trying-to-program <fer.rojitas.abad2001@.com>
2025-10-15 19:19:23 -0500

    Juego piedra papel y tijera funcionando, word con casos subidos

--- Juego/juego.js
+++ Juego/juego.js
@@ -4,9 +4,11 @@ jugar=function(selecionando){
     let suerte=generarElemento();
     let imagen=generarRuta(suerte);
     let ganador=determinarGanador(selecionando,suerte);
+    CambiarTexto("txtMaquina","Tu rival:")
     CambiarImg("imgMaquina",imagen);
     if(ganador==0){
     CambiarTexto("txtResultado","Empate");
+    CambiarImg("imgMaquina","./imagenes/Empate.png")
 }else if(ganador==1){
     CambiarTexto("txtResultado","Gansate");
     puntosUsuario=puntosUsuario+1;
@@ -19,17 +21,35 @@ jugar=function(selecionando){
     if(puntosComputador==5){
         CambiarTexto("txtResultado","El computador te ha vencido");
         document.getElementById("botonOculto").style.display="block";
+        CambiarImg("imgPapel","");
+        CambiarImg("imgPiedra","");
+        CambiarImg("imgTijera","");
+        CambiarImg("imgMaquina","");
+        CambiarTexto("txtMaquina","");
+        CambiarTexto("titulo","");
+
     }else if(puntosUsuario==5){
         CambiarTexto("txtResultado","Has vencido a la computadora");
         document.getElementById("botonOculto").style.display="block";
+        CambiarImg("imgPapel","");
+        CambiarImg("imgPiedra","");
+        CambiarImg("imgTijera","");
+        CambiarImg("imgMaquina","");
+        CambiarTexto("txtMaquina","");
+        CambiarTexto("titulo","");
         
     }
 }
 limpiar=function(){
+    CambiarTexto("txtPuntosUsuario","Puntos: 0");
+    CambiarTexto("txtPuntosMaquina","En contra: 0");
+     document.getElementById("botonOculto").style.display="none";
+     CambiarTexto("titulo","Piedra papel o tijera");
+    CambiarImg("imgPapel","./imagenes/papel.png");
+    CambiarImg("imgPiedra","./imagenes/piedra.png");
+    CambiarImg("imgTijera","./imagenes/tijera.png");
     CambiarImg("imgMaquina","");
     CambiarTexto("txtResultado","");
-    CambiarTexto("txtPuntosUsuario","");
-    CambiarTexto("txtPuntosMaquina","");
     puntosUsuario=0;
     puntosComputador=0;
 }
