e5a974e6bb2f73287bbf5bbafea11e6ff0ee5ee8
Fer-trying-to-program <fer.rojitas.abad2001@.com>
2025-10-20 23:25:40 -0500

    El juego ahorcado funcionando hasta el paso 4

--- Juego ahorcado/ahorcado.js
+++ Juego ahorcado/ahorcado.js
@@ -1,30 +1,62 @@
 let palabraSecreta;
-guardarPalabra=function(){
+ingresarLetra=function(){
+  let letra=recuperarTexto("txtLetra");
+    if(letra>="A" && letra<="Z"){
+        validar(letra);
+    }else{
+        alert("Solo se aceptan mayusculas")
+    }
+
+}
+validar=function(letra){
+    let letrasEncontradas
+    let barridoLetra
+    for(let i=0;i<palabraSecreta.length;i++){
+        barridoLetra=palabraSecreta.charAt(i);
+        if(letra==barridoLetra){
+        mostrarLetra(letra,i);
+        letrasEncontradas=+1
+    }
+}
+}
+mostrarLetra=function(letra,posicion){
+    if(posicion==0){
+        mostrarTexto("div0",letra);
+    }else if(posicion==1){
+        mostrarTexto("div1",letra);
+    }else if(posicion==2){
+        mostrarTexto("div2",letra);
+    }else if(posicion==3){
+        mostrarTexto("div3",letra);
+    }else if(posicion==4){
+        mostrarTexto("div4",letra);
+    }
+}
+
+    guardarPalabra=function(){
     let guardarTxt =recuperarTexto("txtSecreta")
     let longitud=guardarTxt.length
     let letra
     let mayuscula;
-    if(longitud=!5){
+    let comprobarMayuscula=true
+    if(longitud>5 | longitud<5){
         alert("Debe tener 5 caracteres");
-    }else{
+    }else if(longitud==5){
     for(let i=0;i<guardarTxt.length;i++){
         letra=guardarTxt.charAt(i);
         mayuscula=esMayuscula(letra);
         longitud=5;
-        if(mayuscula==true){
-            mayuscula=true;
-        }else{
-            mayuscula=false;
+        if(mayuscula==false){
+            comprobarMayuscula=false;
         }
     }
 }
-
-    if(longitud==5 && mayuscula==true){
+    if(longitud==5 && comprobarMayuscula==true){
         palabraSecreta=guardarTxt
-    }else if(longitud==false){
+    }else if(comprobarMayuscula==false){
         alert("Debe tener 5 Mayusculas");
     }
-}
+    }
 esMayuscula=function(caracter){
     if(caracter>="A" && caracter<="Z"){
         return true;
@@ -32,5 +64,6 @@ esMayuscula=function(caracter){
         return false;
     }
 }
+
 
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
