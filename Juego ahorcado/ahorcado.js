25e2d726879fb51c2e9d7ea8eab06d1f46f18c59
Fer-trying-to-program <fer.rojitas.abad2001@.com>
2025-10-21 00:01:09 -0500

    El juego ahorcado funciona correctamente

--- Juego ahorcado/ahorcado.js
+++ Juego ahorcado/ahorcado.js
@@ -1,23 +1,59 @@
 let palabraSecreta;
+let intentosJugador=0;
+let coincidencias=0;
+let errores=0;
 ingresarLetra=function(){
+    intentosJugador=intentosJugador+1
   let letra=recuperarTexto("txtLetra");
     if(letra>="A" && letra<="Z"){
         validar(letra);
     }else{
         alert("Solo se aceptan mayusculas")
     }
-
+if(coincidencias==5){
+    mostrarImagen("ahorcadoImagen","./ganador.gif");
+}else if(intentosJugador==10){
+    mostrarImagen("ahorcadoImagen","./gameOver.gif");
+}
+}
+mostrarAhorcado=function(){
+    if(errores==1){
+        mostrarImagen("ahorcadoImagen","./Ahorcado_01.png");
+    }else if(errores==2){
+        mostrarImagen("ahorcadoImagen","./Ahorcado_02.png");
+    }else if(errores==3){
+        mostrarImagen("ahorcadoImagen","./Ahorcado_03.png");
+    }else if(errores==4){
+        mostrarImagen("ahorcadoImagen","./Ahorcado_04.png");
+    }else if(errores==5){
+        mostrarImagen("ahorcadoImagen","./Ahorcado_05.png");
+    }else if(errores==6){
+        mostrarImagen("ahorcadoImagen","./Ahorcado_06.png");
+    }else if(errores==7){
+        mostrarImagen("ahorcadoImagen","./Ahorcado_07.png");
+    }else if(errores==8){
+        mostrarImagen("ahorcadoImagen","./Ahorcado_08.png");
+    }else if(errores==9){
+        mostrarImagen("ahorcadoImagen","./Ahorcado_09.png");
+    }
 }
 validar=function(letra){
-    let letrasEncontradas
-    let barridoLetra
+    let letrasEncontradas=false;
+    let barridoLetra;
     for(let i=0;i<palabraSecreta.length;i++){
         barridoLetra=palabraSecreta.charAt(i);
         if(letra==barridoLetra){
         mostrarLetra(letra,i);
-        letrasEncontradas=+1
+        letrasEncontradas=true;
+        coincidencias=coincidencias+1    
     }
 }
+if(letrasEncontradas==false){
+    errores=errores+1;
+    mostrarAhorcado()
+    alert("La letra no es parte de la palabra");
+}
+
 }
 mostrarLetra=function(letra,posicion){
     if(posicion==0){
